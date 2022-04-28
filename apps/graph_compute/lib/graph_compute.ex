b13a388aa61a218f6b1f9c1429cfe130f4bad6f1
RabiaNoumanSiddiq <99790208+RabiaNoumanSiddiq@users.noreply.github.com>
2022-04-28 10:58:53 +0500

    Delete apps/graph_compute directory

--- apps/graph_compute/lib/graph_compute.ex
+++
@@ -1,3 +0,0 @@
-defmodule GraphCompute do
-
-end
--- apps/graph_compute/test/graph_compute_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule GraphComputeTest do
-  use ExUnit.Case
-  # doctest GraphCompute
-
-  # test "greets the world" do
-  #   assert GraphCompute.hello() == :world
-  # end
-end
